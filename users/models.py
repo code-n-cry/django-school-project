8731addcefb8aa2ce19ea9c7ced827ee0e955713
Егор Щукин <e.v.schukin@yandex.ru>
2023-04-04 20:28:49 +0300

    Update user model; squash migrations

--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
 import django.db.models
+import django.utils.html
+import sorl
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy
 
 import teams.models
+
+
+def avatar_image_path(instance, filename):
+    return f'uploads/{instance.user.id}/{filename}'
 
 
 class User(AbstractUser):
@@ -30,8 +37,42 @@ class User(AbstractUser):
         verbose_name='приглашения',
         help_text='куда вас пригласили?',
     )
+    avatar = django.db.models.ImageField(
+        upload_to=avatar_image_path,
+        verbose_name='аватарка',
+        help_text='картинка профиля пользователя',
+        null=True,
+        blank=True,
+    )
+    failed_logins = django.db.models.IntegerField(
+        verbose_name='количество неудачных входов с момента удачного',
+        help_text='сколько раз был провален вход в аккаунт',
+        default=0,
+    )
+    last_failed_login_date = django.db.models.DateTimeField(
+        verbose_name='дата последней неудачной попытки входа',
+        null=True,
+    )
 
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
         default_related_name = 'user'
+
+    def get_avatar_300x300(self):
+        return sorl.thumbnail.get_thumbnail(
+            self.avatar, '300x300', crop='center', quality=65
+        )
+
+    def avatar_tmb(self):
+        if self.avatar:
+            return django.utils.html.mark_safe(
+                f'<img src="{self.get_avatar_300x300().url}">'
+            )
+        self.avatar_tmb.short_description = 'превью'
+        return gettext_lazy('Нет аватарки')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.avatar:
+            self.avatar = self.get_avatar_300x300()
