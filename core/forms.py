96f672bb586831dc23473e7c45d0e195f81ecf38
sagadav <artursagadeev97@gmail.com>
2023-04-11 23:02:52 +0500

    basetailwindform dark theme

--- core/forms.py
+++ core/forms.py
@@ -12,8 +12,8 @@ class BaseTailwindForm(forms.Form):
                     {
                         'class': (
                             'block w-full rounded-md bg-white '
-                            'border-0 py-1.5 pl-7 pr-20 '
-                            'text-gray-900 ring-1 '
+                            'dark:bg-neutral-800 border-0 py-1.5 pl-5 pr-20 '
+                            'text-gray-900 dark:text-white ring-1 '
                             'ring-inset ring-gray-300 '
                             'placeholder:text-gray-400 '
                             'focus:ring-2 focus:ring-inset '
