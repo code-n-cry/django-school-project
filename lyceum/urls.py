aad7f2e57890c32e0db5f6c0bfc8045e63cb8575
sagadav <artursagadeev97@gmail.com>
2023-04-17 16:02:57 +0500

    tasks urls folder

--- lyceum/urls.py
+++ lyceum/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('users/', include('users.urls.users')),
     path('auth/', include('django.contrib.auth.urls')),
     path('meetings/', include('tasks.urls.meetings')),
+    path('tasks/', include('tasks.urls.tasks')),
     path('teams/', include('teams.urls')),
-    path('tasks/', include('tasks.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
